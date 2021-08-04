5a855a030ff8279126c18d726e7d5ad2bf1aabe6
laszlogulyas.jr@gmail.com <laszlogulyas.jr@gmail.com>
2021-08-04 14:19:44 +0200

    angular project replaced

--- frontend/angular-nginx/src/app/app.component.spec.ts
+++ frontend/angular-nginx/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-docker-sandbox'`, () => {
+  it(`should have as title 'angular-nginx'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-docker-sandbox');
+    expect(app.title).toEqual('angular-nginx');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-docker-sandbox app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-nginx app is running!');
   });
 });
